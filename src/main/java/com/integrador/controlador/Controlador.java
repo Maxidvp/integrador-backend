a939dc60e45fa5fc72f77eabe95778ed54ea1e73
Rocabado Cristian <cmrocabado@gmail.com>
2021-12-16 21:50:13 -0300

    "Creacion del master"

---
+++ src/main/java/com/integrador/controlador/Controlador.java
@@ -0,0 +1,16 @@
+package com.integrador.controlador;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controlador {
+	@RequestMapping("/barra")
+	public String barra () {
+		return "Hola vengo del controlador pero con barra";
+	}
+	@RequestMapping
+	public String respuesta () {
+		return "Hola vengo del controlador";
+	}
+}
